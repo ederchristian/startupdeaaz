0bf50aa9cf946629007bc6c0c037177a8d07b9d6
Eder Christian <ederchristian92@gmail.com>
2017-04-28 15:04:41 -0300

    Add scroll for a link to the button on header

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,14 @@ $(document).ready(function() {
 		interval: false,
 	});
 });
+
+/* SCROLL */
+$('a[href^="#"]').on('click', function(event) {
+	var target = $(this.getAttribute('href'));
+	if (target.length) {
+		event.preventDefault();
+		$('html, body').stop().animate( {
+			scrollTop: target.offset().top
+		}, 1000);
+	}
+});
